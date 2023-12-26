c18509568e79d9d0a1cc9381dda19eed08835c1d
Dhairya Bansal <153341045+Dhairya27x@users.noreply.github.com>
2023-12-26 19:26:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+let currentPlayer = 'X';
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+const cells = document.querySelectorAll('.cell');
+
+cells.forEach(cell => {
+    cell.addEventListener('click', handleCellClick);
+});
+
+function handleCellClick(event) {
+    const index = Array.from(cells).indexOf(event.target);
+
+    if (gameBoard[index] === '' && !checkWinner()) {
+        gameBoard[index] = currentPlayer;
+        event.target.textContent = currentPlayer;
+
+        if (checkWinner()) {
+            alert(`${currentPlayer} wins!`);
+        } else if (!gameBoard.includes('')) {
+            alert('It\'s a draw!');
+        } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
+    }
+}
+
+function checkWinner() {
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]             // Diagonals
+    ];
+
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern;
+        if (gameBoard[a] !== '' && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function resetGame() {
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
+    cells.forEach(cell => {
+        cell.textContent = '';
+    });
+    currentPlayer = 'X';
+}
+
+// Create the board elements dynamically
+const boardElement = document.getElementById('board');
+for (let i = 0; i < 9; i++) {
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+    boardElement.appendChild(cell);
+}
+
+// Add reset button
+const resetButton = document.createElement('button');
+resetButton.textContent = 'Reset Game';
+resetButton.addEventListener('click', resetGame);
+document.body.appendChild(resetButton);
